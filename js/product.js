8f653c606aa128d901df6acdcbcbfad2ee8fd7e0
kamoliddin <kuliyevkamoliddin5@gmail.com>
2023-05-31 16:26:50 +0500

    added animation

--- js/product.js
+++ js/product.js
@@ -3,7 +3,7 @@ const productToHtml = () => {
 
   for (let i of product) {
     product_Html.push(`
-            <div class="product__card">
+            <div class="product__card" data-aos="flip-up" data-aos-duration="1500">
                 <img src="${i.image}" alt="">
                 <div class="product__cost">
                     <h2>${i.title}</h2>
